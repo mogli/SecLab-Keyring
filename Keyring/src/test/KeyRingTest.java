747a60d3e895a90cc0dfa759a6c92a76f8f3642a
Martin Weidner <martin.weidner@student.kit.edu>
2011-06-21 00:30:28 +0200

    New test package

---
+++ Keyring/src/test/KeyRingTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KeyRingTest {
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPut() {
+		fail("Not yet implemented");
+	}
+
+}
