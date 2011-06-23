46ebe87f48f6eebbbbc8f00bce751c0988fa8039
Martin Weidner <martin.weidner@student.kit.edu>
2011-06-23 11:47:18 +0200

    new files...

---
+++ Keyring/src/firstSteps/DesUtil.java
@@ -0,0 +1,41 @@
+package firstSteps;
+
+import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.KeySpec;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
+
+import org.bouncycastle.util.encoders.Base64;
+
+
+public class DesUtil {
+	Cipher ecipher;
+	Cipher dcipher;
+	byte[] salt = { (byte) 0xA9, (byte) 0x9B, (byte) 0xC8, (byte) 0x32,
+			(byte) 0x56, (byte) 0x35, (byte) 0xE3, (byte) 0x03 };
+
+	public DesUtil(String passPhrase) throws Exception {
+		int iterationCount = 2;
+		KeySpec keySpec = new PBEKeySpec(passPhrase.toCharArray(), salt,
+				iterationCount);
+		SecretKey key = SecretKeyFactory.getInstance("PBEWithMD5AndDES")
+				.generateSecret(keySpec);
+		ecipher = Cipher.getInstance(key.getAlgorithm());
+		dcipher = Cipher.getInstance(key.getAlgorithm());
+		AlgorithmParameterSpec paramSpec = new PBEParameterSpec(salt,
+				iterationCount);
+		ecipher.init(Cipher.ENCRYPT_MODE, key, paramSpec);
+		dcipher.init(Cipher.DECRYPT_MODE, key, paramSpec);
+	}
+	public byte[] encrypt(byte[] str) throws Exception {
+		return Base64.encode(ecipher.doFinal(str));
+	}
+
+	public byte[] decrypt(byte[] str) throws Exception {
+		return dcipher.doFinal(Base64.decode(str));
+	}
+}
---
+++ Keyring/src/test/DESCodeTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import firstSteps.DesUtil;
+
+public class DESCodeTest {
+	@Test
+	public void try1(){
+		try {
+			DesUtil encryptor = new DesUtil("Hallo Welt");
+			byte[] toEncrypt = "Test".getBytes();
+			byte[] cypher = encryptor.encrypt(toEncrypt);
+			System.out.println(new String(cypher));
+			byte[] cmp = encryptor.decrypt(cypher);
+			System.out.println(new String(cmp));
+			assertArrayEquals(toEncrypt, cmp);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
